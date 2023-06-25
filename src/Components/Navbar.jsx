45e5eeb4dcd7f96466e950fe4b93c65f3b63e9a8
Reinaldocoronel <reinaldocoronel1708@gmail.com>
2023-06-25 17:07:33 -0400

    Home page vertical slice

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "../assets/css/Navbar.css";
+import { FaBars } from "react-icons/fa";
+import {IoMdClose} from "react-icons/io";
+import { Link } from "react-router-dom";
+
+export default function Navbar(){ 
+  return (
+    <>
+        <div className="nav-container">
+                <nav>
+                    <Link to="/"><img src="../public/Images/Jupiter-Logo-small.png" alt="Jupiter SaaS Consulting"/></Link>
+                    <Link to="/Methods">Methods</Link>
+                    <Link to="/ChatGPT_Bard">ChatGPT & Bard</Link>
+                    <Link to="/Integrations">Integrations</Link>
+                    <Link to="/Services">Services</Link>
+                    <Link to="/Bootcamp">Bootcamp</Link>
+                    <Link to="/Team">Team</Link>
+                </nav>
+        </div>
+    </>
+    );
+};
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import '../assets/CSS/HomePage.css';
-
+import '../assets/CSS/fonts.css';
+import Navbar from '../Components/Navbar';
+import { Link } from "react-router-dom";
 
 export default function HomePage(){
   return (
     <>
-      <div className="home-page">
-      <h1>home</h1>
+      <div className="home-page-Grid">
+         <div className="background-world"> 
+         <img src="../public/Images/Jupiter-planet-background.png"/>
+         </div>
+        <div className="hero-third-section">
+        <h1>Optimize your Vacancy-to-Placement Pipeline</h1>
+        <p>The Human-First Approach to Digital Transformation</p>
+        <Link to="" class="ButtonLarge">Let’s Meet!</Link>
+        </div>
       </div>
+      <Navbar/>
     </>
   );
 };
